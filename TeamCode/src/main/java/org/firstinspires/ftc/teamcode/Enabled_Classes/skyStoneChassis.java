1f924824be95500d730a37ca29b3f1186c0f4d68
Hi-TechHornets <48063058+Hi-TechHornets@users.noreply.github.com>
2019-09-15 16:04:32 -0400

    Basic Chassis Control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enabled_Classes/skyStoneChassis.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Enabled_Classes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class skyStoneChassis extends LinearOpMode {
+    private skyStoneRobotControl hth3;
+
+    public void runOpMode() {
+        hth3 = new skyStoneRobotControl();
+
+        hth3.init(hardwareMap);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            hth3.moveDriveMotors(gamepad1.left_stick_y, gamepad1.right_stick_y);
+
+            if(gamepad1.dpad_right) {
+                hth3.moveDriveMotors(1.0, -1.0, -1.0, 1.0);
+            }
+
+            if(gamepad1.dpad_left) {
+                hth3.moveDriveMotors(-1.0, 1.0, 1.0, -1.0);
+            }
+        }
+    }
+}
